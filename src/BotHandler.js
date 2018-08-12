11b36f86d67d0c0693e0aeba08993a94ba29c3ec
Andree Gibson <Andree.Gibson@gmail.com>
2018-08-11 19:11:21 -0500

    Refactored a bit. Using discord.js instead of discord.io now. Delete function added to the bot.

--- src/BotHandler.js
+++ src/BotHandler.js
@@ -1,15 +1,32 @@
-module.exports.handle = (msg, discordData) => {
-  args = msg.substring(1).split(' ');
+logger = require('winston');
+auth = require('../auth.json')
 
-  // get the command from the message to the bot, remove from additional args
-  //cmd = args.splice(1);
+// configure logger
+logger.remove(logger.transports.Console);
+logger.add(new logger.transports.Console, {colorize : true});
+logger.level = 'debug';
 
-  switch(args[0]) {
+module.exports.handle = (msg, bot) => {
+  // get the command from the message to the bot and remove command from args
+  const args = msg.content.slice(auth.prefix.length).trim().split(/ +/g);
+  const cmd = args.shift().toLowerCase();
+
+  logger.debug('args == ' + args)
+
+  logger.debug('Received command [' + cmd + ']')
+
+  switch(cmd) {
     case 'info':
-      discordData.bot.sendMessage({
-        to: discordData.channelID,
-        message: "Welcome to the BotSquad " + discordData.user + "!"
-      });
+      msg.channel.send('Welcome to the BotSquad ' + msg.author + '!');
+      msg.delete();
+      break;
+    case 'delete':
+      logger.debug('deleting message in [' + args[0] + '] seconds');
+      msg.delete(args[0] * 1000);
+      break;
+    default:
+      msg.channel.send('Beep boop... I don\'t understand the command [' + cmd + '].');
+      msg.delete();
       break;
   }
 };
--- src/bot.js
+++ src/bot.js
@@ -1,4 +1,4 @@
-Discord = require('discord.io');
+Discord = require('discord.js');
 logger = require('winston');
 auth = require("../auth.json");
 handler = require('./BotHandler.js');
@@ -9,25 +9,17 @@ logger.add(new logger.transports.Console, {colorize : true});
 logger.level = 'debug';
 
 // initialize Discord bot
-botFlag = '!'; // the flag for operating bot commands
-bot = new Discord.Client({
-  token : auth.token,
-  autorun : true
-});
+const botFlag = auth.prefix; // the flag for operating bot commands
+bot = new Discord.Client();
 bot.on('ready', (event) => {
-  logger.info('Connected to server...');
+  logger.info('Connected to channel... ' ); // TODO - figure out serverId/name and log it
   logger.info('Logged in as: ' + bot.username + '-(' + bot.id +')');
 });
-bot.on('message', (user, userID, channelID, message, event) => {
-
-  discordData = {
-    "user" : user,
-    "userID" : userID,
-    "channelID" : channelID,
-    "bot" : bot
-  };
-  if(message.substring(0,1) == botFlag) {
-    handler.handle(message, discordData);
-    logger.info('message handled');
+bot.on('message', (message) => {
+  if(message.content.substring(0,1) === botFlag) {
+    // handle the message
+    handler.handle(message, bot);
   }
 });
+
+bot.login(auth.token);
